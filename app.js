d331e8709fb5b03bcbe72cf56fe8b271804f6a0b
Emran-Ali <emranitmbstu@gmail.com>
2022-02-11 22:17:39 +0600

    Piano with javaScript

---
+++ app.js
@@ -0,0 +1,70 @@
+
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyA") {
+        let audio = new Audio("A.mp3");
+        audio.play();
+        console.log("The 'A' key is pressed.");
+    }
+    else if (event.code === "KeyS") {
+        // console.log("The 'S' key is pressed.");
+        let audio = new Audio("S.mp3");
+        audio.play();
+        }
+    else if (event.code == "KeyD") {
+        // console.log("The 'D' key is pressed.");
+        let audio = new Audio("D.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyF") {
+        // console.log("The 'F' key is pressed.");
+        let audio = new Audio("F.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyG") {
+        // console.log("The 'G' key is pressed.");
+        let audio = new Audio("G.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyH") {
+        // console.log("The 'H' key is pressed.");
+        let audio = new Audio("H.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyJ") {
+        // console.log("The 'J' key is pressed.");
+        let audio = new Audio("J.mp3");
+        audio.play();
+    }
+    else if (event.code == "KeyW") {
+        let audio = new Audio("W.mp3");
+        audio.play();
+        console.log("The 'W' key is pressed.");
+    }
+     else if (event.code == "KeyE") {
+        let audio = new Audio("E.mp3");
+        audio.play();
+        console.log("The 'E' key is pressed.");
+    }
+    else if (event.code == "KeyT") {
+        let audio = new Audio("T.mp3");
+        audio.play();
+        console.log("The 'T' key is pressed.");
+    }
+    else if (event.code == "KeyY") {
+        let audio = new Audio("Y.mp3");
+        audio.play();
+        console.log("The 'Y' key is pressed.");
+    }
+    else if (event.code == "KeyU") {
+        let audio = new Audio("U.mp3");
+        audio.play();
+        console.log("The 'U' key is pressed.");
+    }
+    // else
+    // {
+    //     console.log(alert("Wrong key"));
+    // }
+
+});
